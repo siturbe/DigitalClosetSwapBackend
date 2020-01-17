949645176357b4ef9e46886c54848a539af4d110
ChilorioX <santiago.iturbe@gmail.com>
2020-01-17 14:51:37 -0600

    Added audience to Auth0Provider to see if then Heroku works

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,9 @@ import { Auth0Provider } from './contexts/auth0-context';
 
 
 ReactDOM.render(
-    <Auth0Provider>
+    <Auth0Provider
+    audience={'https://enigmatic-chamber-52603.herokuapp.com'}
+    >
         <App />
     </Auth0Provider>, 
     document.getElementById('root')
